6bdf0d39214c1fe161f98afa9a8024b7e97e653b
uknowbeers <uknowbeers@gmail.com>
2023-02-20 11:36:07 -0500

    Update Junit

--- test/com/acme/EnrolleesTest.java
+++ test/com/acme/EnrolleesTest.java
@@ -3,6 +3,7 @@ package com.acme;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -18,8 +19,24 @@ public class EnrolleesTest {
     
     @Test
     public void testEnrolleeSorting() {
-//        List<Enrollee> enrolleeList = sut.readEnrolleesFromFile("src/data/enrollees.csv");
-//        assertEquals("Doe", enrolleeList.get(0).getLastName());
+
+        Enrollee enrollee1 = new Enrollee("1", "Jim", "Zebra", "1", "Aetna");
+        Enrollee enrollee2 = new Enrollee("2", "Zychinski", "Jim", "1", "Aetna");
+        Enrollee enrollee3 = new Enrollee("5", "Jon", "Pecc", "1", "Aetna");
+        Enrollee enrollee4 = new Enrollee("79", "Peco", "Jonathan", "1", "Aetna");
+        Enrollee enrollee5 = new Enrollee("999", "Sarah", "Aly", "1", "Aetna");
+
+        List<Enrollee> myList = new ArrayList<>();
+        myList.add(enrollee1);
+        myList.add(enrollee2);
+        myList.add(enrollee3);
+        myList.add(enrollee4);
+        myList.add(enrollee5);
+
+        sut.sortEnrolleesByLastFirst(myList);
+
+        assertEquals("Aly", myList.get(0).getLastName());
+        assertEquals("Zebra", myList.get(4).getLastName());
     }
     
 //    @Test
